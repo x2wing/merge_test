9c9d8d96ca64ec9f273327ddbb9acadf16c261a1
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2019-11-17 20:11:08 +0500

    checker_r added

--- utils.py
+++ utils.py
@@ -6,3 +6,10 @@ def checker(V):
         if V[i - 1] > V[i]:
             print("сортировка говно")
             return
+
+
+def checker_r(V, ascending=False):
+    for i in range(1, len(V)):
+        if ascending ^ (V[i-1] > V[i]):
+            print("сортировка говно")
+            return
